3d2dd89c0286fce59af748812d54c7af51bbe00e
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-04-14 12:52:43 +0530

    [shravanthi] First Commit from master

---
+++ First.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloWorldInCSharp
+{
+    public class First
+    {
+        public int Id;
+        public static void One()
+        {
+            Console.WriteLine("Public");
+        }
+        private static void Two()
+        {
+            Console.WriteLine("Private");
+        }
+        internal static void Three()
+        {
+            Console.WriteLine("internal");
+        }
+        protected static void Four()
+        {
+            Console.WriteLine("protected");
+        }
+    }
+    public class Second:First
+    {
+        public int x = 100;
+        public static int y = 200;
+
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using helloWorldInCSharp;
+
+First first = new();
+first.Id = 123;
+Console.WriteLine(first.Id);
+First.One();
+First.Three();
+//First.Two();
+//First.Four();
+Console.WriteLine(Second.y);//static variable, so no need of instance
+Second second= new();
+Console.WriteLine(second.x);//non-static variable, accesiing through instance
+const float pi = 3.14f;
+Console.WriteLine(pi);
